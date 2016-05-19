7a3af8c2f4ba655a3f3b5ff727278502c621c943
PERRIN Olivier <perrinolivier88@gmail.com>
2016-05-19 15:29:42 +0200

    v1

---
+++ Contacts.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html class=" desktop landscape" lang="en"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<style type="text/css">.gm-style .gm-style-mtc label,.gm-style .gm-style-mtc div{font-weight:400}</style>
+<style type="text/css">.gm-style-pbc{transition:opacity ease-in-out;background-color:black;text-align:center}.gm-style-pbt{font-size:22px;color:white;font-family:Roboto,Arial,sans-serif;position:relative;margin:0;top:50%;-webkit-transform:translateY(-50%);-ms-transform:translateY(-50%);transform:translateY(-50%)}</style>
+<link href="Contacts_fichiers/css.css" rel="stylesheet" type="text/css">
+<style type="text/css">.gm-style .gm-style-cc span,.gm-style .gm-style-cc a,.gm-style .gm-style-mtc div{font-size:10px}</style><style type="text/css">@media print {  .gm-style .gmnoprint, .gmnoprint {    display:none  }}@media screen {  .gm-style .gmnoscreen, .gmnoscreen {    display:none  }}</style>
+<!-- /*<style type="text/css">.gm-style{font-family:Roboto,Arial,sans-serif;font-size:11px;font-weight:400;text-decoration:none}.gm-style img{max-width:none}</style>*/ -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="format-detection" content="telephone=no">
+  <title>Contacts</title>
+
+  <!-- Bootstrap -->
+  <link href="Contacts_fichiers/bootstrap.css" rel="stylesheet">
+
+  <!-- Links -->
+  <link rel="stylesheet" href="Contacts_fichiers/rd-mailform.css">
+  <link rel="stylesheet" href="font-awesome-4.6.3/css/font-awesome.min.css">
+  <!--JS-->
+  <script src="Contacts_fichiers/visits.json" defer="true" async=""></script>
+  <script src="Contacts_fichiers/linkid.js" async="" type="text/javascript"></script>
+  <script style="" src="Contacts_fichiers/analytics.js" async="" type="text/javascript"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  
+  <!--[if lt IE 9]>
+  <div style=' clear: both; text-align:center; position: relative;'>
+    <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+      <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+           alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+    </a>
+  </div>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
+  <script src="Contacts_fichiers/device.js"></script>
+<script src="Contacts_fichiers/common.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/map.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/util.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/marker.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/infowindow.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/onion.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/stats.js" charset="UTF-8" type="text/javascript"></script>
+<script src="Contacts_fichiers/controls.js" charset="UTF-8" type="text/javascript"></script>
+<script data-cfasync="false" defer="defer" async="" src="Contacts_fichiers/application2.js"></script>
+<style type="text/css">.olark-key,#hbl_code,#olark-data{display: none !important;}</style>
+<link href="Contacts_fichiers/cbb092d6554938a9549cb716ffcb1f94.css" rel="stylesheet" type="text/css" id="habla_style_div">
+<style type="text/css">#olk-b-2250 .cancel-button-olk-6808,#olk-b-2250 .referral-button-olk-6808{font-family:"Helvetica Neue",HelveticaNeue,Helvetica,Arial,sans-serif !important;font-size:14px !important;font-weight:bold !important;border-radius:3px !important;border:1px solid #d65129;-webkit-font-smoothing:antialiased;width:80% !important;padding:10px 15px !important;margin-bottom:10px !important;transition:.03s;}#olk-b-2250 .cancel-button-olk-6808:hover,#olk-b-2250 .referral-button-olk-6808:hover{border:1px solid #c14925}#olk-b-2250 .cancel-button-olk-6808:focus,#olk-b-2250 .referral-button-olk-6808:focus{outline:0}#olk-b-2250{width:100% !important;display:inline-block !important}#olk-b-2250 .wrapper-olk-6808{background:#fff;border-left:1px solid #ededed;border-right:1px solid #ededed;text-align:center;width:100%;position:absolute;top:34px;right:0;bottom:0;left:0;padding:10px;box-sizing:border-box;overflow-y:auto}#olk-b-2250 .description-olk-6808{font-size:14px;font-family:"Helvetica Neue",HelveticaNeue,Helvetica,Arial,sans-serif;line-height:1.4em;margin:0 0 15px}#olk-b-2250 .cancel-button-olk-6808{background:#d65129;color:#fff;}#olk-b-2250 .cancel-button-olk-6808:hover{background:#c14925}#olk-b-2250 .referral-button-olk-6808{background:#fff;color:#d65129;}#olk-b-2250 .referral-button-olk-6808:hover{color:#c14925}#olk-b-2250 .brand-olk-6808{color:#a6aeb0 !important;font-family:"Helvetica Neue",HelveticaNeue,Helvetica,Arial,sans-serif !important;padding:0 !important;font-size:11px !important;margin:0 0 5px 10px !important;display:inline-block !important;transition:.03s;}#olk-b-2250 .brand-olk-6808:hover{color:#d65129 !important}</style><style type="text/css">@media print {#habla_beta_container_do_not_rely_on_div_classes_or_names {display: none !important}}</style></head>
+<body><div id="olark" style="display: none;"><olark><iframe id="olark-loader" frameborder="0"></iframe></olark></div>
+<div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+<header>
+        <div id="stuck_container" class="stuck_container">
+            <div class="container container-wide">
+
+                <nav class="navbar navbar-default navbar-static-top ">
+
+                    <div class="navbar-header center-xs">
+                        <h1 class="navbar-brand">
+                            <a href="index.html">Clerio.fr</a>
+                        </h1>
+                    </div>
+
+
+                    <ul class="navbar-nav sf-menu navbar-right sf-js-enabled sf-arrows" data-type="navbar">
+                        <li class="active">
+                            <a href="index.html">Accueil</a>
+                        </li>
+                        <li>
+                            <a href="commander.html">Commande</a>
+                        </li>
+                        <li>
+                            <a href="Contacts.php">Nous Contacter</a>
+                        </li>
+
+                    </ul>
+                </nav>
+      </div>
+    </div>
+
+  <div style="top: -58px; visibility: hidden; position: fixed; width: 100%; margin-top: 0px;" id="stuck_container" class="stuck_container isStuck">
+      <div class="container container-wide">
+        <nav class="navbar navbar-default navbar-static-top ">
+          <div class="navbar-header center-xs">
+            <h1 class="navbar-brand">
+              <a href="#">Japan House</a>
+            </h1>
+          </div>
+          <ul class="navbar-nav sf-menu navbar-right sf-js-enabled sf-arrows" data-type="navbar">
+            <li class="">
+              <a href="#">Home</a>
+            </li>
+            <li class="">
+              <a href="#">About us</a>
+            </li>
+            <li class="">
+              <a href="#">Specialities</a>
+            </li>
+
+            <li class="dropdown">
+              <a class="sf-with-ul" href="#">Menu</a>
+              <ul style="display: none;" class="dropdown-menu">
+                <li>
+                  <a href="#">Nigiri and Sashimi</a>
+                </li>
+
+
+                <li>
+                  <a href="#">Vegetarian Rolls</a>
+                </li>
+                <li>
+                  <a href="#">Gunkan</a>
+                </li>
+                <li>
+                  <a class="sf-with-ul" href="#">Sushi Combination</a>
+                  <ul style="display: none;" class="dropdown-menu">
+                    <li>
+                      <a href="#">Sushi Set</a>
+                    </li>
+                    <li>
+                      <a href="#">Rolls</a>
+                    </li>
+                    <li>
+                      <a href="#">Nigiri Set</a>
+                    </li>
+                    <li>
+                      <a href="#">404</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">Our Speciality Rolls</a>
+                </li>
+                <li>
+                  <a href="#">Kitchen Dishes</a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="">
+              <a href="#">Services</a>
+            </li>
+
+            <li class="active">
+              <a href="#">Contact us</a>
+            </li>
+
+          </ul>
+        </nav>
+      </div>
+    </div></header>
+
+
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+
+  <main>
+    <!-- CONTACT US -->
+    <section class="well-12 bg-image-left bg-image-left--4">
+      <div class="container container-wide text-center">
+        <h2 class="head-bord-4">
+                    <span class="small">
+                        Lorem ipsum
+                      </span>
+                      CONTACTER NOUS
+        </h2>
+
+        <div class="row center-sm text-left offset-2">
+          <div class="col-md-3">
+            <address class="contact-info">
+              <p>8901 Marmora Road <br>
+                Glasgow, DO4 89GR.</p>
+              <dl>
+                <dt>Tel:</dt>
+                <dd><a href="callto:#">959 603 6030</a></dd>
+              </dl>
+
+              <a href="mailto:#">mail@demolink.org</a>
+            </address>
+          </div>
+          <div class="col-md-6">
+            <!-- Php -->
+            <?php
+              $url = "exemple@gmail.com";
+              $nom = $_POST['nom'];
+              $prenom = $_POST['prenom'];
+              $mail = $_POST['email'];
+              $message = $_POST['message'];
+              $post = $_POST['postal'];
+              $vil = $_POST['ville'];
+              $tel = $_POST['tel'];
+              mail($url.''.$prenom.''.$nom.''.$message.''.$post.''.$vil.''.$tel.''.$mail);
+            ?>
+            <!--Fin Php-->
+            <form class="rd-mailform" method="post" action="Contacts.php">
+              <!-- RD Mailform Type -->
+              <input name="form-type" value="contact" type="hidden">
+              <!-- END RD Mailform Type -->
+
+              <div class="form-group mfInput">
+                <input id="fname" name="nom" type="text" placeholder="Prenom" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="prenom" type="text" placeholder="Nom" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="tel" type="text" placeholder="Telephone" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="ville" type="text" placeholder="Ville" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="fname" name="postal" type="text" placeholder="Code Postal" class="form-control"required></div>
+
+              <div class="form-group mfInput">
+                <input id="inputEmail" name="email" type="email" placeholder="Adresse mail" class="form-control"required></div>
+
+
+              <div class="form-group textarea mfInput">
+                <textarea class="form-control" id="message" name="message" placeholder="Message" rows="7"required></textarea></div>
+
+              <div class="form-group btn-wr">
+                <button class="btn btn-lg btn-primary" type="submit">Envoyer</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END CONTACT US -->
+  </main>
+
+  <!--========================================================
+                          FOOTER
+  =========================================================-->
+  <footer>
+    <section>
+      <div class="container container-wide center-sm">
+        <div class="row">
+          <div class="col-md-5 ">
+            <p class="rights">
+              © <span id="copyright-year">2016</span> Clerio, TOUT LES DROITS RESERVER.
+            </p>
+          </div>
+          <div class="col-md-2 text-center ">
+            <a href="#" class="brand-name"> Clerio</a>
+          </div>
+          <div class="col-md-offset-2 col-md-3 ">
+            <ul class="inline-list">
+              <li><a href="#" class="icon icon-md icon-default fa-facebook"></a></li>
+              <li><a href="#" class="icon icon-md icon-default  fa-pinterest-p"></a></li>
+              <li><a href="#" class="icon icon-md icon-default fa-twitter"></a></li>
+              <li><a href="#" class="icon icon-md icon-default fa-google-plus"></a></li>
+              <li><a href="#" class="icon icon-md icon-default fa-instagram"></a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </footer>
+</div>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script> <!-- begin olark code --> <script data-cfasync="false" type='text/javascript'>/*<![CDATA[*/ window.olark || (function (c) {     var f = window, d = document, l = f.location.protocol == "https:" ? "https:" : "http:", z = c.name, r = "load";     var nt = function () {         f[z] = function () {             (a.s = a.s || []).push(arguments)         };         var a = f[z]._ = {}, q = c.methods.length;         while (q--) {             (function (n) {                 f[z][n] = function () {                     f[z]("call", n, arguments)                 }             })(c.methods[q])         }         a.l = c.loader;         a.i = nt;         a.p = {             0: +new Date         };         a.P = function (u) {             a.p[u] = new Date - a.p[0]         };         function s() {             a.P(r);             f[z](r)         }          f.addEventListener ? f.addEventListener(r, s, false) : f.attachEvent("on" + r, s);         var ld = function () {             function p(hd) {                 hd = "head";                 return ["<", hd, "></", hd, "><", i, ' onl' + 'oad="var d=', g, ";d.getElementsByTagName('head')[0].", j, "(d.", h, "('script')).", k, "='", l, "//", a.l, "'", '"', "></", i, ">"].join("")             }              var i = "body", m = d[i];             if (!m) {                 return setTimeout(ld, 100)             }             a.P(1);             var j = "appendChild", h = "createElement", k = "src", n = d[h]("div"), v = n[j](d[h](z)), b = d[h]("iframe"), g = "document", e = "domain", o;             n.style.display = "none";             m.insertBefore(n, m.firstChild).id = z;             b.frameBorder = "0";             b.id = z + "-loader";             if (/MSIE[ ]+6/.test(navigator.userAgent)) {                 b.src = "javascript:false"             }             b.allowTransparency = "true";             v[j](b);             try {                 b.contentWindow[g].open()             } catch (w) {                 c[e] = d[e];                 o = "javascript:var d=" + g + ".open();d.domain='" + d.domain + "';";                 b[k] = o + "void(0);"             }             try {                 var t = b.contentWindow[g];                 t.write(p());                 t.close()             } catch (x) {                 b[k] = o + 'd.write("' + p().replace(/"/g, String.fromCharCode(92) + '"') + '");d.close();'             }             a.P(2)         };         ld()     };     nt() })({     loader: "static.olark.com/jsclient/loader0.js",     name: "olark",     methods: ["configure", "extend", "declare", "identify"] }); /* custom configuration goes here (www.olark.com/documentation) */ olark.identify('7830-582-10-3714'); /*]]>*/</script> <noscript><a href="https://www.olark.com/site/7830-582-10-3714/contact" title="Contact us" target="_blank">Questions?     Feedback?</a> powered by <a href="http://www.olark.com?welcome" title="Olark live chat software">Olark live chat     software</a></noscript> <!-- end olark code -->
+</body><!-- Google Tag Manager --><noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N7VWVN" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push( {'gtm.start': new Date().getTime(),event:'gtm.js'} );var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-N7VWVN');</script> <!-- End Google Tag Manager -->
